8f116c00513fd62a760d5b818435c01ced225fd0
girmabetela <girmabetela119@gmail.com>
2026-08-12 20:13:01 +0300

    Set up React application with KidsCalculator component

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import KidsCalculator from "./App.jsx";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <KidsCalculator />
+  </React.StrictMode>
+);
